2a1f70e4c66eeac913587f7a78b64c1c225049a5
Jevon Tyler <jevont17@gmail.com>
2020-09-03 07:45:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
+=======
+var greetSpecialGuest = "Hello," + specialGuest + "!";
+>>>>>>> 479323f4c2791432374a75462ab969cc76edbdbd
 
 var topic = "space";
 
